8e20537d2895a561c3f23ca4ee5e542cc5f0516b
Shifu Chen <chen@haplox.com>
2017-08-17 20:47:50 +0800

    refine gen_fusion_file.jl

--- scripts/gen_fusion_file.jl
+++ scripts/gen_fusion_file.jl
@@ -23,10 +23,15 @@ function main(args)
 end
 
 function detect(ref, genes, fusion)
+    ref = lowercase(ref)
+    if !(ref in ["hg19", "grch37", "grch38"])
+        println("reference <-f/--ref> should be one of hg19/grch37/grch38")
+        return
+    end
     input = open(genes)
     output = open(fusion, "w")
     genestr = readstring(input)
-    genelist = split(genestr)
+    allgenes = split(genestr)
 
     println("loading gencode " * ref)
     index = gencode_load(ref)
@@ -34,28 +39,31 @@ function detect(ref, genes, fusion)
 
     processed_genes = []
 
-    for genestr in genelist
-        gene = strip(genestr, ',')
-        gene = uppercase(gene)
-        gene_matches = gencode_genes(index, gene)
-        if length(gene_matches)==0
-            println("skipped ", gene, ", gene name not found in gencode")
-            continue
+    for genestr in allgenes
+        genelist = split(genestr, ',')
+        for gene in genelist
+            gene = uppercase(gene)
+            gene_matches = gencode_genes(index, gene)
+            if length(gene_matches)==0
+                println("----skipped ", gene, ", gene name not found in gencode")
+                continue
+            end
+            if gene in processed_genes
+                println("----skipped ", gene, ", duplicated")
+                continue
+            end
+            println(gene)
+            g = gene_matches[1]
+            write(output, ">", g.name, ",", g.chr, ":", string(g.start_pos), "-", string(g.end_pos), "\n")
+            exons = g.transcripts[1].exons
+            id = 1
+            for e in exons
+                write(output, string(id), ",", string(e.start_pos), "-", string(e.end_pos), "\n")
+                id += 1
+            end
+            write(output, "\n")
+            push!(processed_genes, gene)
         end
-        if gene in processed_genes
-            println("skipped ", gene, ", duplicated")
-            continue
-        end
-        g = gene_matches[1]
-        write(output, ">", g.name, ",", g.chr, ":", string(g.start_pos), "-", string(g.end_pos), "\n")
-        exons = g.transcripts[1].exons
-        id = 1
-        for e in exons
-            write(output, string(id), ",", string(e.start_pos), "-", string(e.end_pos), "\n")
-            id += 1
-        end
-        write(output, "\n")
-        push!(processed_genes, gene)
     end
     flush(output)
 end
