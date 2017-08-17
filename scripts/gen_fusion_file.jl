aac90e234205ce3b09161f71a4f215299041ab27
Shifu Chen <chen@haplox.com>
2017-08-17 20:33:00 +0800

    add scripts/gen_fusion_file.jl to generate fusion.csv

---
+++ scripts/gen_fusion_file.jl
@@ -0,0 +1,65 @@
+using OpenGene
+using OpenGene.Reference
+using ArgParse
+
+function main(args)
+
+    s = ArgParseSettings(description = "gene a fusion CSV file for given gene list")
+
+    @add_arg_table s begin
+        "--genes",  "-g"
+            help = "the file giving gene list (separated by comma, space or line break)"
+            required = true
+        "--ref",  "-r"
+            help = "the reference genome to use (hg19/hg37/grch38)"
+            required = true
+        "--fusion",   "-f"
+            help = "the fusion file to output"
+            required = true
+    end
+
+    options = parse_args(s) # the result is a Dict{String,Any}
+    detect(options["ref"], options["genes"], options["fusion"])
+end
+
+function detect(ref, genes, fusion)
+    input = open(genes)
+    output = open(fusion, "w")
+    genestr = readstring(input)
+    genelist = split(genestr)
+
+    println("loading gencode " * ref)
+    index = gencode_load(ref)
+    println("loading done")
+
+    processed_genes = []
+
+    for genestr in genelist
+        gene = strip(genestr, ',')
+        gene = uppercase(gene)
+        gene_matches = gencode_genes(index, gene)
+        if length(gene_matches)==0
+            println("skipped ", gene, ", gene name not found in gencode")
+            continue
+        end
+        if gene in processed_genes
+            println("skipped ", gene, ", duplicated")
+            continue
+        end
+        g = gene_matches[1]
+        write(output, ">", g.name, ",", g.chr, ":", string(g.start_pos), "-", string(g.end_pos), "\n")
+        exons = g.transcripts[1].exons
+        id = 1
+        for e in exons
+            write(output, string(id), ",", string(e.start_pos), "-", string(e.end_pos), "\n")
+            id += 1
+        end
+        write(output, "\n")
+        push!(processed_genes, gene)
+    end
+    flush(output)
+end
+
+# REPL is not supported
+
+main(ARGS)
