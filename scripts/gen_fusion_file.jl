cf45b957b6267c9c924a90bda46b81c7c138494c
Shifu Chen <chen@haplox.com>
2017-08-29 21:09:45 +0800

    update gen_fusion script

--- scripts/gen_fusion_file.jl
+++ scripts/gen_fusion_file.jl
@@ -39,32 +39,67 @@ function detect(ref, genes, fusion)
 
     processed_genes = []
 
+    passed_num = 0
     for genestr in allgenes
-        genelist = split(genestr, ',')
-        for gene in genelist
-            gene = uppercase(gene)
+        genelist = split(genestr, '-')
+        for genename in genelist
+            if genename in processed_genes
+                #println("----skipped ", genename, ", already processed")
+                continue
+            end
+            gene_attrs = split(genename, '_')
+            gene = gene_attrs[1]
+            transcript = ""
+            if length(gene_attrs)>1
+                transcript = gene_attrs[2]
+            end
             gene_matches = gencode_genes(index, gene)
             if length(gene_matches)==0
-                println("----skipped ", gene, ", gene name not found in gencode")
+                #println("----skipped ", genename, ", gene name not found in gencode")
                 continue
             end
-            if gene in processed_genes
-                println("----skipped ", gene, ", duplicated")
-                continue
-            end
-            println(gene)
+            println(genename)
             g = gene_matches[1]
-            write(output, ">", g.name, ",", g.chr, ":", string(g.start_pos), "-", string(g.end_pos), "\n")
-            exons = g.transcripts[1].exons
+            write(output, ">", genename, ",", g.chr, ":", string(g.start_pos), "-", string(g.end_pos), "\n")
+
+            # get the correct transcript if it is specified
+            target_tr = nothing
+            if transcript!=""
+                for tr in g.transcripts
+                    if startswith(tr.id, transcript)
+                        target_tr = tr
+                        break
+                    end
+                end
+                if target_tr == nothing
+                    #println("----skipped ", genename, ", transcript not found")
+                end
+            end
+
+            # get the longest transcript if it is not specified
+            longest = 0
+            if target_tr == nothing
+                for tr in g.transcripts
+                    if length(tr.exons)>longest
+                        longest = length(tr.exons)
+                        target_tr = tr
+                        break
+                    end
+                end
+            end
+
+            exons = target_tr.exons
             id = 1
             for e in exons
                 write(output, string(id), ",", string(e.start_pos), ",", string(e.end_pos), "\n")
                 id += 1
             end
+            passed_num += 1
             write(output, "\n")
-            push!(processed_genes, gene)
+            push!(processed_genes, genename)
         end
     end
+    println("successfully processed genes: ", passed_num)
     flush(output)
 end
 
